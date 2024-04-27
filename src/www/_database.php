353b451ad33a3934a6457e63db10b64382da1e30
xa4os34 <matyushag8907@gmail.com>
2024-04-27 20:10:34 +0500

    Added funcitons to interact with posts

--- src/www/_database.php
+++ src/www/_database.php
@@ -1,4 +1,8 @@
 <?php
+// ini_set('display_startup_errors', 1);
+// ini_set('display_errors', 1);
+// error_reporting(-1);
+
 $options = parse_ini_file("php.ini");
 $connection = new mysqli(
     $options["host"],
@@ -79,8 +83,97 @@ function GetUserByUsername(string $username) : ?User {
         passwordHash: $result[3]);
 }
 
+function GetUserPosts(int $userId) : ?array {
+    global $connection;
+        
+    $user = GetUserById($userId);
+
+    if ($user == null)
+        return null;
+
+    $result = $connection->query(
+        "SELECT Id, Title, Content, TitleImage FROM Posts WHERE OwnerId = '{$userId}'");
+
+    if ($result == null)
+        return null;
+
+    $posts = array();
+
+    while($row = $result->fetch_assoc()) {
+        array_push($posts, new Post(
+            $row['Id'], $row['Title'], 
+            $row['Content'], $row['TitleImage'],
+            $user));
+    }
+    
+    return $posts;
+}
+
+function GetPosts() : ?array {
+    global $connection;
+
+    $result = $connection->query(
+        "SELECT post.Id, post.Title, post.Content, post.TitleImage, user.Id, user.Username, user.Email
+             FROM Posts AS post
+                  LEFT JOIN Users AS user 
+                  ON post.OwnerId = user.Id
+         ORDER BY post.Title;");
+
+    if ($result == null)
+        return null;
+
+    $posts = array();
+
+    while($row = $result->fetch_row()) {
+        array_push($posts, new Post(
+            $row[0], $row[1], 
+            $row[2], $row[3],
+            new User (
+                $row[5], $row[6], 
+                null, $row[4])));
+    }
+    
+    return $posts;
+}
+
+function GetPostById(int $postId) : ?array {
+    global $connection;
+
+    $result = $connection->query(
+        "SELECT post.Id, post.Title, post.Content, post.TitleImage, user.Id, user.Username, user.Email
+             FROM Posts AS post
+                 LEFT JOIN Users AS user 
+                 ON post.OwnerId = user.Id
+         WHERE post.Id = {$postId};");
+
+    if ($result == null)
+        return null;
+
+    $row = $result->fetch_row();
+
+    return new Post(
+        $row[0], $row[1], 
+        $row[2], $row[3],
+        new User (
+            $row[5], $row[6], 
+            null, $row[4]));
+}
+
+function CreatePost(string $title, string $content, ?string $TitleImage, int $ownerId) : ?int {
+    global $connection;
+    // Very readable code. I think...
+    $result = $connection->query(
+        "INSERT INTO Posts (Title, Content, " . ($TitleImage != null ? "TitleImage, " : "") . "OwnerId) 
+         VALUES ('{$title}', '{$content}', " . ($TitleImage != null ? " '{$TitleImage}', " : "") . "'{$ownerId}');");
+
+    if (!$result)
+        return null;
+
+    return $connection->insert_id; 
+}
+
 class User {
-    public readonly ?int $Id;
+    public readonly int $Id;
     public readonly string $Username;
     public readonly string $Email;
     public readonly string $PasswordHash;
@@ -91,6 +184,26 @@ class User {
         $this->Email = $email;
         $this->PasswordHash = $passwordHash;
         $this->Id = $id;
+    }
+}
+
+class Post {
+    public readonly int $Id;
+    public readonly string $Title;
+    public readonly string $Content;
+    public readonly string $TitleImage;
+    public readonly User $Owner;
+    
+    public function __construct(
+        int $id, string $title, 
+        string $content, string $titleImage,
+        User $owner){
+        
+        $this->Id = $id;
+        $this->Title = $title;
+        $this->Content = $content;
+        $this->TitleImage = $titleImage;
+        $this->Owner = $owner;
     }
 }
 
