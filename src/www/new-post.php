e277bb74f9ccb33a0b7542c860f587b135359006
xa4os34 <matyushag8907@gmail.com>
2024-04-28 15:15:12 +0500

    Added new post page. Yeah, I forgot about postCard.php.

---
+++ src/www/new-post.php
@@ -0,0 +1,54 @@
+<?php 
+
+session_start();
+
+include '_database.php';
+include '_utils.php';
+
+AuthorizationRequired();
+
+$validationErrors = array();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {   
+    CsrfTokenRequired();
+
+    $titleImage = null;
+    $title = ParamRequired("title");
+    $content = ParamRequired("content");
+
+    if (strlen($content) > 2000)
+        array_push($validationErrors, "Content must be less then 2000 characters long.");
+
+    if (strlen($title) > 100)
+        array_push($validationErrors, "Title must be less then 100 characters long.");
+
+    if (isset($_FILES["titleImage"]) && intval($_FILES["titleImage"]["size"]) > 0) {
+        $fileInfo = $_FILES["titleImage"];
+        if ($fileInfo["size"] > 1048576) {
+            array_push($validationErrors, "Size of Title Image must be less then 1MiB.");
+            goto renderPage;
+        }
+
+        $titleImage = basename($fileInfo["name"]);
+        move_uploaded_file($fileInfo["tmp_name"], getcwd() . "/files/title-images/" . $titleImage);
+    }
+
+    if (count($validationErrors) > 0)
+        goto renderPage;   
+
+    $userId = $_SESSION["user_id"];
+    $postId = CreatePost($title, $content, $titleImage, $userId);
+
+    if ($postId == null)
+        die();
+
+    header("HTTP/1.1 302 Found");
+    header("Location: /posts?id={$postId}");
+    exit;
+}
+
+renderPage:
+$title = 'New post';
+$mainBody = 'views/_new-post.php';
+include '_layout.php';
+?>
---
+++ src/www/views/_new-post.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <form class="form" enctype="multipart/form-data" method="POST">
+        <p class="text-center fs-1">New post</p>
+        <div class="mb-4" style="max-width: 30rem;">
+            <label for="inputTitle" class="form-label fs-4">Title</label>
+            <input required type="text" class="form-control text-bold" name="title"
+                id="inputTitle" value="<?php echo $_POST["title"]?>">
+        </div>
+        <div class="mb-4" style="max-width: 30rem;">
+            <label for="inputTitleImage" class="form-label fs-4">Title image</label>
+            <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
+            <input class="form-control" type="file" name="titleImage" id="inputTitleImage">
+        </div>
+        <div class="mb-4">
+            <label for="inputContent" class="form-label fs-4">Content</label>
+            <textarea requird style="min-height: 30rem;" class="form-control" type="text" name="content" 
+                maxlength="2000" id="inputContent" value="<?php echo $_POST["content"]?>">
+            </textarea>
+        </div>
+        <div class="md-3">
+        <?php foreach ($validationErrors as $error) { ?>
+            <p class="text-danger"><?php echo $error; ?></p>
+        <?php }?>
+        </div>
+        <div class="mb-4">
+            <input hidden type="text" name="csrf" value="<?php echo GetCsrfToken();?>">
+            <input required type="submit" class="btn btn-primary" value="Create" name="submit">
+        </div>
+    </form>
+<div>
