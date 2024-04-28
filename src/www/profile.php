01e64785b84788928afec5d5c810395eddb854bc
xa4os34 <matyushag8907@gmail.com>
2024-04-28 15:12:00 +0500

    added profile page

--- src/www/profile.php
+++ src/www/profile.php
@@ -3,9 +3,11 @@
 session_start();
 
 include '_utils.php';
+include '_database.php';
 
 AuthorizationRequired();
 
+$posts = GetUserPosts($_SESSION["user_id"]);
 $title = "Profile";
 $mainBody = "views/_profile.php";
 include '_layout.php';
--- src/www/views/_profile.php
+++ src/www/views/_profile.php
@@ -2,4 +2,19 @@
     <p class="fs-1 mb-4">Profile</p>
     <p class="fs-3 mb-4">Username: <?php echo $_SESSION['username']; ?></p>
     <p class="fs-3 mb-4">Email: <?php echo $_SESSION['email']; ?></p>
+    <p class="fs-3 mb-4">Your posts:</p>
+    <hr>
+    <div class="d-flex flex-wrap gap-3">
+        <?php 
+        foreach($posts as $post) {
+            $post = $post;
+            include 'views/_postCard.php';
+        }
+        ?>
+        <div class="btn card text-center position-relative bg-dark bg-opacity-50 border border-3" style="width: 18rem; height: 26rem">
+            <a class="btn fs-5 btn-secondary position-absolute top-50 start-50 translate-middle" href="/new-post">
+                Write a new post!
+            </a>
+        </div>
+    </div>
 <div>
